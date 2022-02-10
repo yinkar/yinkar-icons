51635e13bcd5db5d2dd61bc1dd6d9edf4eaf2eb2
Yinkar <hkrabs@yandex.com>
2022-02-11 00:39:37 +0300

    Updates

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  base: 'https://yinkar.github.io/toys/yinkar-icons/'
+  base: '/yinkar-icons/',
+  plugins: [vue()]
 })
