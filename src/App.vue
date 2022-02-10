3ee14581115bc4929cbd2c32d0338fe39d7b8b0d
Yinkar <hkrabs@yandex.com>
2022-02-10 09:22:24 +0300

    Initialization

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Icons from './components/Icons.vue';
+</script>
+
+<template lang="pug">
+div#container
+  h1 Yinkar Icons
+  Icons
+</template>
+
+<style lang="stylus">
+@import url('@fontsource/open-sans')
+
+body
+  font-family 'Open Sans', sans-serif
+
+#container
+  text-align center
+</style>
