701d92d7e3129850ba60f39afb08bba8f5c95b0a
Yinkar <hkrabs@yandex.com>
2022-02-12 22:06:46 +0300

    Vite PWA integration

--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 
+import { registerSW } from 'virtual:pwa-register';
+
 import './index.css';
+
+const updateSW = registerSW({
+    onNeedRefresh() {},
+    onOfflineReady() {},
+});
+
+if (typeof window !== 'undefined') {
+    import('./pwa');
+}
 
 createApp(App).mount('#app');
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,44 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/yinkar-icons/',
-  plugins: [vue()],
-  build: {
-    manifest: true,
-  },
+  plugins: [
+    vue(),
+    VitePWA({
+      workbox: {
+        cleanupOutdatedCaches: false,
+        sourcemap: true,
+      },
+      includeAssets: [
+        'favicon.svg',
+        'robots.txt',
+        'icon-192.png',
+        'icon-512.png',
+      ],
+      manifest: {
+          "name": "Yinkar Icons",
+          "short_name": "YinkarIcons",
+          "description": "A tiny icon set",
+          "theme_color": "#374151",
+          "background_color": "#222",
+          "icons": [
+              {
+                  "src": "icon-192.png",
+                  "sizes": "192x192",
+                  "type": "image/png",
+                  "purpose": "any maskable",
+              },
+              {
+                  "src": "icon-512.png",
+                  "sizes": "512x512",
+                  "type": "image/png",
+                  "purpose": "any maskable",
+              }
+          ],
+      },
+    }),
+  ],
 });
